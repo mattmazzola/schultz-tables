a41bc027e109381d5e94dedad35f40abdb35f9b9
Matt Mazzola <mattmazzola@live.com>
2023-01-08 20:10:53 -0800

    Start on scores page

--- client-remix/app/routes/scores.tsx
+++ client-remix/app/routes/scores.tsx
@@ -1,23 +1,87 @@
-import { DataFunctionArgs, json } from "@remix-run/node"
+import { DataFunctionArgs, json, LinksFunction } from "@remix-run/node"
 import { useLoaderData } from "@remix-run/react"
 import { auth } from "~/services/auth.server"
+import { IScore, IScoresResponse } from "~/types/models"
+import * as options from '~/utilities/options'
+import scoresStyles from "~/styles/scores.css"
+
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: scoresStyles },
+]
 
 export const loader = async ({ request }: DataFunctionArgs) => {
   const profile = await auth.isAuthenticated(request, {
     failureRedirect: "/"
   })
+  const scoreTypeToScores: Record<string, IScoresResponse> = {}
 
   return json({
     profile,
+    scoreTypeToScores,
   })
 }
 
 export default function Scores() {
-  const { profile } = useLoaderData<typeof loader>()
-  
+  const { profile, scoreTypeToScores } = useLoaderData<typeof loader>()
+
   return (
-    <div>
-      <h1>Scores</h1>
-    </div>
-  );
+    <>
+      <h2 className="scoresHeader">Scores by Table Type:</h2>
+      <div className="scoresColumns">
+        {options.presetTables.map(gameType => {
+          const scoresByType = scoreTypeToScores[gameType.id]
+          return (
+            <div key={gameType.id} className="scoreColumn">
+              <div className="scoreColumn__header">
+                {/* <GameType
+                  key={gameType.id}
+                  gameType={gameType}
+                  onClick={() => { }}
+                /> */}
+              </div>
+              <div className="scoreRefreshButton">
+                <button disabled={scoresByType == null} >
+                  Refresh Top 50
+                </button>
+              </div>
+              <div>
+                {(scoresByType?.scores ?? []).length === 0
+                  ? <div className="score">0 Scores</div>
+                  : (scoresByType?.scores ?? []).map((s, i) => {
+                    let rank: string = `${i + 1}th`
+                    if (i === 0) {
+                      rank = '1st 🥇'
+                    }
+                    else if (i === 1) {
+                      rank = '2nd 🥈'
+                    }
+                    else if (i === 2) {
+                      rank = `3rd 🥉`
+                    }
+                    else if (i === 3) {
+                      rank = `4th 🌟`
+                    }
+                    else if (i === 4) {
+                      rank = `5th ⭐`
+                    }
+
+                    const name = s.user
+                      ? (s.user.nickname ?? s.user.name ?? s.user.email)
+                      : s.userId
+                    return (
+                      <div key={s.id} className="score">
+                        <div>🏁</div><div>{rank}</div>
+                        <div>⌚</div><div>{(s.durationMilliseconds / 1000).toFixed(2)} sec</div>
+                        <div>⏲</div><div>{s.startTime}</div>
+                        <div>🧑</div><div className="scoreName" title={name}>{name}</div>
+                      </div>
+                    )
+                  })}
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </>
+  )
 }
