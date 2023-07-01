6dbf82817938f3bbd5e4d92cdd917e1c0eee468b
Matt Mazzola <mattmazzola@live.com>
2023-06-30 21:45:51 -0700

    Update remix conf

--- client-remix/remix.config.js
+++ client-remix/remix.config.js
@@ -5,4 +5,12 @@ module.exports = {
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
+  serverModuleFormat: "cjs",
+  future: {
+    v2_errorBoundary: false,
+    v2_headers: false,
+    v2_meta: false,
+    v2_normalizeFormMethod: false,
+    v2_routeConvention: false,
+  },
 };
